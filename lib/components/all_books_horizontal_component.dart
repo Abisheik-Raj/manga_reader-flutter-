e8556307de2cf245227a320b9fc210b7caada6dc
Abisheik-Raj <dgodly2004@gmail.com>
2024-06-18 08:23:29 +0530

    refactored the entire all books horizontal component

--- lib/components/all_books_horizontal_component.dart
+++ lib/components/all_books_horizontal_component.dart
@@ -11,6 +11,7 @@ class AllBooksHorizontalComponent extends StatelessWidget {
     required this.description,
     required this.publicDemographic,
   });
+
   String imageUrl;
   String title;
   String contentRating;
@@ -23,71 +24,65 @@ class AllBooksHorizontalComponent extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: screenSize.height * 0.22,
-            width: screenSize.width * 0.32,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              image: DecorationImage(
-                  fit: BoxFit.cover, image: NetworkImage(imageUrl)),
-              borderRadius: BorderRadius.circular(20),
+      child: SizedBox(
+        width: double.infinity,
+        height: screenSize.height * 0.22,
+        child: Row(
+          children: [
+            Container(
+              width: screenSize.width * 0.32,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: NetworkImage(imageUrl)),
+                borderRadius: BorderRadius.circular(20),
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-          SizedBox(
-            width: screenSize.width * 0.55,
-            child: Flexible(
+            const SizedBox(
+              width: 15,
+            ),
+            Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  RichText(
-                    text: TextSpan(
-                      text: title,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontFamily: "PoppinsRegular",
-                        fontSize: 18,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: "\n$contentRating  $publicDemographic",
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontFamily: "PoppinsRegular",
-                              fontSize: 12,
-                              height: 2.2),
-                        ),
-                      ],
+                  Text(
+                    title,
+                    maxLines: 2,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontFamily: "PoppinsRegular",
+                      fontSize: 18,
+                      overflow: TextOverflow.ellipsis,
                     ),
                   ),
-                  // const Icon(
-                  //   Icons.bookmark_border_outlined,
-                  //   color: Colors.white,
-                  //   size: 26,
-                  // ),
                   Text(
-                    "\n\n$description",
-                    maxLines: 7,
+                    "$contentRating  $publicDemographic",
+                    maxLines: 1,
                     style: const TextStyle(
                         color: Colors.grey,
                         fontFamily: "PoppinsRegular",
                         fontSize: 12,
-                        overflow: TextOverflow.ellipsis),
+                        height: 2.2),
                   ),
                   const SizedBox(
-                    height: 6,
+                    height: 25,
+                  ),
+                  Expanded(
+                    child: Text(
+                      description,
+                      maxLines: 5,
+                      style: const TextStyle(
+                          color: Colors.grey,
+                          fontFamily: "PoppinsRegular",
+                          fontSize: 12,
+                          overflow: TextOverflow.ellipsis),
+                    ),
                   ),
                 ],
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
